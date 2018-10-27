f4dd0bec30f2548d598340f3efaec7a0696a010c
hd <374045465@qq.com>
2018-10-27 11:28:35 +0800

    动画2

---
+++ app/src/main/java/com/example/view/ListView/Animo2Activity.java
@@ -0,0 +1,22 @@
+package com.example.view.ListView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.view.BaseActivity;
+import com.example.view.R;
+
+public class Animo2Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.activity_animo2;
+    }
+
+
+}
